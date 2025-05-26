7b4b1f80eae7a3f8afca70525c4a83a4fb3adc73
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-05-25 19:26:09 -0500

    add delay to toast vanish

--- index.js
+++ index.js
@@ -2,31 +2,28 @@ const toast = document.querySelector(".toast");
 const submitBtn = document.querySelector("#submit-btn");
 
 
+toast.style.opacity = 1;
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     showToast();
 })
 
-
-
-
-
 function showToast() {
     toast.style.display = "flex";
-    toast.style.opacity = 1;
     
     
-    let myInterval = setInterval(vanishToast, 100);
+    setTimeout(() => {
+        let myInterval = setInterval(vanishToast, 100);
     function vanishToast() {
         if (toast.style.opacity == 0) {
             clearInterval(myInterval)
         } else {
             toast.style.opacity = toast.style.opacity - 0.1;
         } 
-}      
+    }      
+    }, 2000);
 }
 
 
 
-console.log("Initial toast opacity: " + toast.style.opacity)
